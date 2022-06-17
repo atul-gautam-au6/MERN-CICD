b5a5be4639057e42adc8a3dc74496d93e072f16d
atul-gautam-au6 <atul.gautam@vayuz.com>
2022-06-17 08:43:46 +0530

    setup mern application

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const path  = require('path')
+const dotenv  = require('dotenv')
+dotenv.config()
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/reactapp/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'reactapp', 'build', 'index.html'))
+  );
+}
+
+
 app.listen(PORT, () => {
   console.log(`Server is runnig on PORT ${PORT}`);
 });
